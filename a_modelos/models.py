8fc9780e1ec3b435ec7f434512608306b55417c5
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-11 12:42:18 +0200

    Actualización modelos y añadí el método __str__

--- a_modelos/models.py
+++ a_modelos/models.py
@@ -6,6 +6,9 @@ class Usuario(models.Model):
     correo_electronico = models.CharField(max_length=50,unique=True)
     contraseña = models.CharField(max_length=20)
     fecha_de_registro = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.nombre    
     
     
     
@@ -27,6 +30,8 @@ class Tarea(models.Model):
     fecha_de_creacion = models.DateField()
     hora_de_vencimiento = models.TimeField()
     
+    def __str__(self) -> str:
+        return self.titulo
 
 class Proyecto(models.Model):
     nombre = models.CharField(max_length=100)
@@ -34,20 +39,29 @@ class Proyecto(models.Model):
     duracion_estimada = models.FloatField()
     fecha_de_inicio = models.DateField()
     fecha_de_finalizacion = models.DateField()
+    usuario = models.ManyToManyField(Usuario)
     
+    def __str__(self) -> str:
+        return self.nombre
 
 class Etiqueta(models.Model):
     nombre = models.CharField(max_length=100)
     
+    def __str__(self) -> str:
+        return self.nombre
     
 class AsignacionTarea(models.Model):
     observaciones = models.TextField()
     fecha_de_asignacion = models.DateTimeField()
     
+    def __str__(self) -> str:
+        return self.observaciones
 
 class Comentario(models.Model):
     contenido = models.TextField()
     fecha_de_contenido = models.DateTimeField()
+    
+    
     
     
 
